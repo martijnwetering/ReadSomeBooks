fd7a9d9b364df3fe335a77b5d845846698d1ab7d
Martijn <vandewetering.m@gmail.com>
2014-05-31 00:49:11 +0200

    Productenoverzicht Refactored productenoverzicht. De boeken worden nu gerenderd met behulp van een functie zodat de html-code hiervoor niet elke keer herhaald hoeft te worden.

--- app/helperFunctions.php
+++ app/helperFunctions.php
@@ -63,4 +63,29 @@ function checkPasswordStrength($password)
     }
 }
 
+// Receives an pdo [$result] and renders the books.
+function renderBooks($result)
+{
+    while ($row = $result->fetch())
+    {
+        $inShoppingCart = isset($_SESSION['cart'][$row['PRODUCTNUMMER']]);
+        ?>
+        <div class="product-klein">
+            <a href="<?php echo $_SERVER['PHP_SELF'] . '?page=product-detail&productId=' . $row['PRODUCTNUMMER']; ?>">
+                <img src="<?php echo $row['AFBEELDING_KLEIN']; ?>" alt="" />
+                <div class="product-info">
+                    <span class="po-naam"><?php echo $row['TITEL']; ?></span>
+                    <span class="po-autheur"><em><?php echo $row['SCHRIJVER']; ?></em></span>
+                </div>
+            </a>
+            <span class="po-prijs"><?php echo $row['PRIJS']; ?></span>
+            <button class="addToShoppingCart <?php if ($inShoppingCart) {echo 'in-winkelwagen';} ?>">
+                <i class="fa fa-shopping-cart fa-2x"></i>
+            </button>
+            <input type="hidden" value="<?php echo $row['PRODUCTNUMMER']; ?>" />
+            <div class="clearfix"></div>
+        </div>
+    <?php
+    }
+}
 ?>
--- views/productenoverzicht.php
+++ views/productenoverzicht.php
@@ -48,77 +48,19 @@
         }
         else
         {
-            while ($row = $searchBookOnTitelOrWriter->fetch())
-            {
-                $inShoppingCart = isset($_SESSION['cart'][$row['PRODUCTNUMMER']]);
-                ?>
-                <div class="product-klein">
-                    <a href="<?php echo $_SERVER['PHP_SELF'] . '?page=product-detail&productId=' . $row['PRODUCTNUMMER']; ?>">
-                        <img src="<?php echo $row['AFBEELDING_KLEIN']; ?>" alt="" />
-                        <div class="product-info">
-                            <span class="po-naam"><?php echo $row['TITEL']; ?></span>
-                            <span class="po-autheur"><em><?php echo $row['SCHRIJVER']; ?></em></span>
-                        </div>
-                    </a>
-                    <span class="po-prijs"><?php echo $row['PRIJS']; ?></span>
-                    <button class="addToShoppingCart <?php if ($inShoppingCart) {echo 'in-winkelwagen';} ?>">
-                        <i class="fa fa-shopping-cart fa-2x"></i>
-                    </button>
-                    <input type="hidden" value="<?php echo $row['PRODUCTNUMMER']; ?>" />
-                    <div class="clearfix"></div>
-                </div>
-            <?php
-            }
+            renderBooks($searchBookOnTitelOrWriter);
         }
     }
-    else if (!isset($_GET['categorie']))
+    // If a categorie is set, this will only render the books in that categorie.
+    else if (isset($_GET['categorie']))
     {
-        $retrieveAllBooks->execute();
-        while ($row = $retrieveAllBooks->fetch())
-        {
-            // Ckecks if an item is already in the shopping cart
-            $inShoppingCart = isset($_SESSION['cart'][$row['PRODUCTNUMMER']]);
-        ?>
-        <div class="product-klein">
-            <a href="<?php echo $_SERVER['PHP_SELF'] . '?page=product-detail&productId=' . $row['PRODUCTNUMMER']; ?>">
-                <img src="<?php echo $row['AFBEELDING_KLEIN']; ?>" alt="" />
-                <div class="product-info">
-                    <span class="po-naam"><?php echo $row['TITEL']; ?></span>
-                    <span class="po-autheur"><em><?php echo $row['SCHRIJVER']; ?></em></span>
-                </div>
-            </a>
-            <span class="po-prijs"><?php echo $row['PRIJS']; ?></span>
-            <button class="addToShoppingCart <?php if ($inShoppingCart) {echo 'in-winkelwagen';} ?>">
-                <i class="fa fa-shopping-cart fa-2x"></i>
-            </button>
-            <input type="hidden" value="<?php echo $row['PRODUCTNUMMER']; ?>" />
-            <div class="clearfix"></div>
-        </div>
-    <?php
-        }
+        $retrieveAllBooksInCategorie->execute(array($_GET['categorie']));
+        renderBooks($retrieveAllBooksInCategorie);
     }
     else
     {
-        $retrieveAllBooksInCategorie->execute(array($_GET['categorie']));
-        while ($row = $retrieveAllBooksInCategorie->fetch())
-        {
-     ?>
-        <div class="product-klein">
-            <a href="<?php echo $_SERVER['PHP_SELF'] . '?page=product-detail' ?>">
-                <img src="<?php echo $row['AFBEELDING_KLEIN']; ?>" alt="" />
-                <div class="product-info">
-                    <span class="po-naam"><?php echo $row['TITEL']; ?></span>
-                    <span class="po-autheur"><em><?php echo $row['SCHRIJVER']; ?></em></span>
-                </div>
-            </a>
-            <span class="po-prijs"><?php echo $row['PRIJS']; ?></span>
-            <button id="addToShoppingCart">
-                <a href="<?php echo $_SERVER['REQUEST_URI'] . '&item=' . $row['PRODUCTNUMMER']; ?>"><i class="fa fa-shopping-cart fa-2x"></i></a>
-            </button>
-            <div class="clearfix"></div>
-        </div>
-    <?php
-        }
+        $retrieveAllBooks->execute();
+        renderBooks($retrieveAllBooks);
     }
     ?>
 </div>
